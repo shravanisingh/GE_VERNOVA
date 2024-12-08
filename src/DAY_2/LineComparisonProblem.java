019550a02c2806ce295a790b8eda49dd188794b6
shravnisingh <shravanisingh22@gmail.com>
2024-12-09 00:34:58 +0530

    added day2 uc6

---
+++ src/DAY_2/LineComparisonProblem.java
@@ -0,0 +1,8 @@
+package src.DAY_2;
+
+public class LineComparisonProblem {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation");
+    }
+}

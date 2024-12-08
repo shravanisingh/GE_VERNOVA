a7c36774fc3dc26192dc5c85b3f59651178bf0c5
shravnisingh <shravanisingh22@gmail.com>
2024-12-08 23:16:13 +0530

    add initial commit for day2

---
+++ src/DAY_2/EmployeeWageComputationProblem.java
@@ -0,0 +1,9 @@
+package src.DAY_2;
+
+public class EmployeeWageComputationProblem {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+    }
+}
+

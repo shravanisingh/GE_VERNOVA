b2ecce733fb354e63c732df3c57f3d417c847714
shravnisingh <shravanisingh22@gmail.com>
2024-12-09 01:14:22 +0530

    added day3

--- src/DAY_3/SnakeAndLadderProblem.java
+++ src/DAY_3/SnakeAndLadderProblem.java
@@ -3,7 +3,5 @@ package src.DAY_3;
 public class SnakeAndLadderProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Game!");
-        System.out.println("Single Player at Start Position 0");
-
     }
 }

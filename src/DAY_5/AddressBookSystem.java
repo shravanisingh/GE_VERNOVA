80c1e4568af28ca6004e82054e0c2aafe6697f20
shravnisingh <shravanisingh22@gmail.com>
2024-12-10 00:25:00 +0530

    added day5 p2 uc2

--- src/DAY_5/AddressBookSystem.java
+++ src/DAY_5/AddressBookSystem.java
@@ -8,42 +8,19 @@ public class AddressBookSystem {
         int i = 1;
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Address book");
-        do{
-            System.out.println("Enter 1 to create contact ");
+        do {
+            System.out.println("Enter 1 to create contact \n Enter 2 to display al contacts");
             int input = sc.nextInt();
-            switch (input){
-                case 1 -> createContact(a1);
+            switch (input) {
+                case 1 -> a1.createContact();
+                case 2 -> a1.display();
                 default -> System.out.println("Wrong input");
             }
             System.out.println("Enter 0 to exit");
             i = sc.nextInt();
 
-        }while(i!=0);
-
+        } while (i != 0);
         System.out.println("Thank you for using address book");
         sc.close();
-
-
     }
-    static void createContact(AddressBook a1) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter name");
-        String name = sc.next();
-        System.out.println("Enter last name");
-        String lname = sc.next();
-        System.out.println("Enter city");
-        String city = sc.next();
-        System.out.println("Enter state");
-        String state = sc.next();
-        System.out.println("Enter email");
-        String email = sc.next();
-        System.out.println("Enter phone");
-        int phone = sc.nextInt();
-        System.out.println("Enter name");
-        int zip = sc.nextInt();
-        Contact c1 = new Contact(name,lname,city,state,email,phone,zip);
-        a1.addContact(c1);
-
-    }
-
 }
